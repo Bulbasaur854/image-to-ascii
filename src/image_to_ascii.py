fccbfcf5bb2f4ba6bca360628077efe499654fc8
Bulbasaur854 <tairshechter1118@gmail.com>
2025-05-22 11:26:06 +0300

    map to ascii and format output

--- src/image_to_ascii.py
+++ src/image_to_ascii.py
@@ -1,15 +1,31 @@
 from PIL import Image
 import math
 
+ASCII_RAMP = [
+    "$", "@", "B", "%", "8", "&", "W", "M", "#", "*", "o", "a", "h", "k", "b", "d",
+    "p", "q", "w", "m", "Z", "O", "0", "Q", "L", "C", "J", "U", "Y", "X", "z", "c",
+    "v", "u", "n", "x", "r", "j", "f", "t", "/", "\\", "|", "(", ")", "1", "{", "}",
+    "[", "]", "?", "-", "_", "+", "~", "<", ">", "i", "!", "l", "I", ";", ":", ",",
+    "\"", "^", "`", "'", ".", " "
+]
+ASCII_RAMP.reverse()
+# ASCII_RAMP = [" ", ".", ":", "-", "=", "+", "*", "#", "%", "@"]
+
 def load_image(filepath):
     return Image.open(filepath)
 
 def resize_image(image, new_width=100):
     width, height = image.size
     ratio = height / width
-    new_height = int(ratio * new_width)   
-
+    new_height = int(ratio * new_width)       
     return image.resize((new_width, new_height))  
 
 def convert_to_grayscale(image):
-    return image.convert("LA")
+    return image.convert("RGBA").convert("L")
+
+def map_pixels_to_ascii(image):
+    pixels = image.getdata()    
+    return "".join([ASCII_RAMP[pixel * (len(ASCII_RAMP) - 1) // 255] for pixel in pixels])
+
+def format_ascii_output(ascii_chars, width=100):
+    return list("".join(ascii_chars[i:i+width]) for i in range(0, len(ascii_chars), width))
--- src/main.py
+++ src/main.py
@@ -1,12 +1,15 @@
 from image_to_ascii import *
 
-def main():
+ASCII_WIDTH = 50
+
+def main():    
     img = load_image("bulbasaur_example.png")
-    print(f"Original Image: {img}")
-    img_resized = resize_image(img)
-    print(f"Resized Image: {img_resized}")
+    img_resized = resize_image(img, ASCII_WIDTH)
     img_gray = convert_to_grayscale(img_resized)
-    print(f"Gray Scaled Image: {img_gray}")
+    img_ascii = map_pixels_to_ascii(img_gray)
+    lines = format_ascii_output(img_ascii, ASCII_WIDTH)
+    for line in lines:
+        print(line)
 
 if __name__ == "__main__":
     main()
