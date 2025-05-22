9a617c56ff09a35bde1b0ff4604b29e437902bbc
Bulbasaur854 <tairshechter1118@gmail.com>
2025-05-22 14:15:25 +0300

    user input width

--- src/main.py
+++ src/main.py
@@ -1,14 +1,13 @@
 from image_to_ascii import *
 
-ASCII_WIDTH = 64
-
 def main():    
     image_path = input("Hello, please enter the path to your image: ")
+    image_width = int(input("And desired output width: "))
     img = load_image(image_path)
-    img_resized = resize_image(img, ASCII_WIDTH)
+    img_resized = resize_image(img, image_width)
     img_gray = convert_to_grayscale(img_resized)
     img_ascii = map_pixels_to_ascii(img_gray)
-    lines = format_ascii_output(img_ascii, ASCII_WIDTH)
+    lines = format_ascii_output(img_ascii, image_width)
     save_or_display_ascii(lines)
 
 if __name__ == "__main__":
