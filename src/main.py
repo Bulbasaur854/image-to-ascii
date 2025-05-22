97f4e0333c41a158392a34478bd1578441909d0c
Bulbasaur854 <tairshechter1118@gmail.com>
2025-05-22 12:40:52 +0300

    user input

--- src/main.py
+++ src/main.py
@@ -3,7 +3,8 @@ from image_to_ascii import *
 ASCII_WIDTH = 64
 
 def main():    
-    img = load_image("shroom.png")
+    image_path = input("Hello, please enter the path to your image: ")
+    img = load_image(image_path)
     img_resized = resize_image(img, ASCII_WIDTH)
     img_gray = convert_to_grayscale(img_resized)
     img_ascii = map_pixels_to_ascii(img_gray)
